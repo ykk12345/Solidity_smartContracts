0faaf2d987dcd02e8b1757432860c802f97bd0d2
Yash kumar kaushik <108153942+ykk12345@users.noreply.github.com>
2023-11-02 23:06:01 +0530

    Create Hello_worlds.sol

---
+++ Hello_worlds.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+// Here SPDX stands for Software Package Data Exchange
+//means who can use this piece of code
+
+pragma solidity ^0.8.0;
+
+contract Hello{
+    string public welcome="Hello world";
+}
