be1791dc00e72439a3faad3ad47a76d1ce061d6f
Yash kumar kaushik <108153942+ykk12345@users.noreply.github.com>
2023-11-02 23:23:25 +0530

    Create Datatypes.sol
    This file contains the sample example of the data types present in the solidity:
    1) address -> which stores the ethereum address
    2) string -> which stores a string (sequence of characters)
    3) boolean -> it is either true or false
    4)uint -> it stores unsigned integer values

---
+++ Datatypes.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract fundamentals{
+    
+    // address data type
+    address myaddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    // string data types
+
+    string message = "Hello solidity";
+
+    // boolean data type
+
+    bool isready = true;
+
+    // uint data type
+
+    uint8 max_supply = 10;
+
+    uint256 public  wait_time  = 1 days;  // 86400
+}
